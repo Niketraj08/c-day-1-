ad23e5208ef2018bfafdd23f0d11a3d32b89be2d
Niketraj08 <niketrajkvs@gmail.com>
2026-06-08 13:50:26 +0530

    leap year

--- assig_ques2.cpp
+++ assig_ques2.cpp
@@ -51,3 +51,35 @@ int main()
 //     }
 //     return 0;
 // }
+
+
+
+
+
+
+// leap year question // #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int year;
+//     cout << "Enter the year :" << endl;
+//     cin >> year;
+//     if (year % 400 == 0)
+//     {
+//         cout << year << " is a leap year.\n";
+//     }
+//     else if (year % 100 == 0)
+//     {
+//         cout << year << " is NOT a leap year.\n";
+//     }
+//     else if (year % 4 == 0)
+//     {
+//         cout << year << " is a leap year.\n";
+//     }
+//     else
+//     {
+//         cout << year << " is NOT a leap year.\n";
+//     }
+//     return 0;
+// }
+ 
