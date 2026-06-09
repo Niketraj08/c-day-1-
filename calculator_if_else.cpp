3b0c3a902462291c8d16757b98809a6bc89c3dac
Niketraj08 <niketrajkvs@gmail.com>
2026-06-09 15:21:57 +0530

    1

--- calculator_if_else.cpp
+++ calculator_if_else.cpp
@@ -41,37 +41,3 @@ int main()
 
 
 
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int a, b;
-//     char op;
-//     cout << "Enter the a number : " << endl;
-//     cin >> a;
-//     cout << "Enter the b number : " << endl;
-//     cin >> b;
-//     cout << "Enter the operator number : " << endl;
-//     cin >> op;
-//     if (op == '+')
-//     {
-//         cout << "a+b =" << (a + b) << endl;
-//     }
-//     else if (op == '-')
-//     {
-//         cout << "a-b =" << (a - b) << endl;
-//     }
-//     else if (op = '*')
-//     {
-//         cout << "a*b =" << (a * b) << endl;
-//     }
-//     else if (op = '/')
-//     {
-//         cout << "a/b =" << (a / b) << endl;
-//     }
-//     else
-//     {
-//         cout << "Invalid number " << endl;
-//     }
-//     return 0;
-// }
