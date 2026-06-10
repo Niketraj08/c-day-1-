080b30451b77b4642e151f8ceb9ac88fa877c4e6
Niketraj08 <niketrajkvs@gmail.com>
2026-06-10 11:09:32 +0530

    1

--- odd_or_even.cpp
+++ odd_or_even.cpp
@@ -42,3 +42,23 @@ int main()
 
 //     return 0;
 // }
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << " Enter the number : " << endl;
+
+    cin >> num;
+
+    if (num % 2 == 0)
+    {
+        cout << "Number is Even" << endl;
+    }
+    else
+    {
+        cout << "Number is Odd" << endl;
+    }
+
+    return 0;
+}
