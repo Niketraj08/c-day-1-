ffb19cf4523669c544c8ea1c6f9778036c7adf8b
Niketraj08 <niketrajkvs@gmail.com>
2026-06-11 17:44:07 +0530

    commit

--- odd_or_even.cpp
+++ odd_or_even.cpp
@@ -22,26 +22,6 @@ int main()
 }
 
 
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int num;
-//     cout << " Enter the number : " << endl;
-
-//     cin >> num;
-
-//     if (num % 2 == 0)
-//     {
-//         cout << "Number is Even" << endl;
-//     }
-//     else
-//     {
-//         cout << "Number is Odd" << endl;
-//     }
-
-//     return 0;
-// }
 #include <iostream>
 using namespace std;
 int main()
